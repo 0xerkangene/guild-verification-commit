531b471df7021241a0ee06bc3b83ccd1b9d4f0d1
Mustafa Erkan GENE <147904401+0xerkangene@users.noreply.github.com>
2025-10-31 15:59:23 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T12:58:54.809Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-10-31T12:59:22.918Z
+Commit: Resolve null pointer exception in user service
